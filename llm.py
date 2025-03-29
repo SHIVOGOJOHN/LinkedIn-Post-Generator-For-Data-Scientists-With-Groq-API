286d5284ae05b7a249743a639a8f761bac87ce6c
JOHN SHIVOGO <jonesjorney@gmail.com>
2025-03-29 18:33:51 +0300

    Update llm.py

--- llm.py
+++ llm.py
@@ -1,15 +1,11 @@
-from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 import os
 import base64
-
-
-load_dotenv(dotenv_path=r"C:\Users\A\.vscode\.vscode\LinkedIn\.env")
+import streamlit as st
 
 # Directly assign API key here for testing
-groq_api_key='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'
+groq_api_key = st.secrets["general"]["GROQ_API_KEY"]
 
-print(os.getenv("GROQ_API_KEY"))
 llm= ChatGroq(groq_api_key=groq_api_key, model_name="llama-3.2-90b-vision-preview")
 
 if __name__=="__main__":
